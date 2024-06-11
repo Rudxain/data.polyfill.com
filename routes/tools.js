10820f4e79165b5fcc01bba5ae7e566b03992b0b
vincent-backend <yutaka@inc-acb.com>
2024-06-11 23:25:11 +0700

    update tools

--- routes/tools.js
+++ routes/tools.js
@@ -1,6 +1,8 @@
 import express from 'express';
 var router = express.Router();
 
+import { google_hostes_libraries } from '../utils/google-hosted-libraries.js';
+
 import dotenv from 'dotenv';
 if (process.env.NODE_ENV !== 'production') {
     dotenv.config({ path: '.env.development' });
@@ -9,35 +11,35 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const github_entry = 'https://github.com/';
+const unpkg_base = 'https://unpkg.com/';
+const google_base = 'https://ajax.googleapis.com/ajax/libs/';
+const skypack_base = 'https://cdn.skypack.dev/';
+const esmsh_base = 'https://esm.sh/';
 
 router.get('/gh', async (req, res) => {
 
-    let gitUrl = req.query.url;
+    let srcUrl = req.query.url;
     let cdnUrl = '';
 
-    if (gitUrl === undefined || gitUrl === '') {
-        res.send({success:false});
+    if (srcUrl === undefined || srcUrl === '') {
+        res.send({ success: false });
         return;
     }
 
-    console.log(gitUrl);
-
-    if (!gitUrl.startsWith(github_entry)) {
-        res.send({success:false});
+    if (!srcUrl.startsWith(github_entry)) {
+        res.send({ success: false });
         return;
     }
-    
+
     /// remove github entry
-    gitUrl = gitUrl.replace(github_entry, "");
-    console.log(gitUrl);
+    srcUrl = srcUrl.replace(github_entry, "");
 
     /// split
-    let splitted = gitUrl.split('/');
-    console.log(splitted);
+    let splitted = srcUrl.split('/');
 
     /// check url again
     if (splitted.length < 5) {
-        res.send({success:false});
+        res.send({ success: false });
         return;
     }
 
@@ -45,16 +47,184 @@ router.get('/gh', async (req, res) => {
     let version = splitted[3];
     if (version === 'main') version = '';
 
-    console.log(splitted);
-
     const filePath = splitted.slice(4).join('/');
 
     cdnUrl = `${process.env.CDN_BASE_URL}/gh/${splitted[0]}/${splitted[1]}@${version}/${filePath}`;
-    console.log(cdnUrl);
 
-    const respData = {success:true, url:cdnUrl};
+    const respData = { success: true, url: cdnUrl };
     res.send(respData);
 })
 
+router.get('/unpkg', async (req, res) => {
+
+    let srcUrl = req.query.url;
+    let cdnUrl = '';
+
+    if (srcUrl === undefined || srcUrl === '') {
+        res.send({ success: false });
+        return;
+    }
+
+    if (!srcUrl.startsWith(unpkg_base)) {
+        res.send({ success: false });
+        return;
+    }
+
+    /// remove github entry
+    srcUrl = srcUrl.replace(unpkg_base, "");
+    console.log(srcUrl);
+
+    /// split
+
+    let splitted = srcUrl.split('/');
+    console.log(splitted);
+    
+    /// check url again
+    if (splitted.length < 2) {
+        res.send({ success: false });
+        return;
+    }
+
+    const v2 = splitted.join('/');
+    console.log(v2);
+
+    cdnUrl = `${process.env.CDN_BASE_URL}/npm/${v2}`;
+    console.log(cdnUrl);
+
+    const respData = { success: true, url: cdnUrl };
+    res.send(respData);
+})
+
+
+router.get('/google', async (req, res) => {
+
+    let srcUrl = req.query.url;
+    let cdnUrl = '';
+
+    if (srcUrl === undefined || srcUrl === '') {
+        res.send({ success: false });
+        return;
+    }
+
+    if (!srcUrl.startsWith(google_base)) {
+        res.send({ success: false });
+        return;
+    }
+
+    /// remove github entry
+    srcUrl = srcUrl.replace(google_base, "");
+
+    /// split
+
+    let splitted = srcUrl.split('/');
+    
+    /// check url again
+    if (splitted.length < 2) {
+        res.send({ success: false });
+        return;
+    }
+
+    /// check if it is google hosted libraries
+    const pkg = splitted[0];
+    const version = splitted[1];
+    const file = splitted.slice(2).join('/');
+
+    const isGoogleHosted = google_hostes_libraries.some(lib => lib.name === pkg);
+    if (isGoogleHosted === false) {
+        res.send({ success: false });
+        return;
+    }
+
+    console.log(file);
+
+    const googleHostedObj = google_hostes_libraries.find(lib=>lib.name===pkg);
+    const isEntryValid = googleHostedObj.entrypoints.some(entry=>entry.google === file);
+    if (isEntryValid === false) {
+        res.send({ success: false });
+        return;
+    }
+
+    const googleEntry = googleHostedObj.entrypoints.find(entry => entry.google === file);
+
+    const npm_package = googleHostedObj.npm_name;
+    const npm_entrypoint = googleEntry.cdn;
+
+    cdnUrl = `${process.env.CDN_BASE_URL}/npm/${npm_package}@${version}/${npm_entrypoint}`;
+
+    const respData = { success: true, url: cdnUrl };
+    res.send(respData);
+})
+
+router.get('/skypack', async (req, res) => {
+
+    let srcUrl = req.query.url;
+    let cdnUrl = '';
+
+    console.log('parsing skypack');
+
+    if (srcUrl === undefined || srcUrl === '') {
+        res.send({ success: false });
+        return;
+    }
+
+    if (!srcUrl.startsWith(skypack_base)) {
+        res.send({ success: false });
+        return;
+    }
+
+    /// remove github entry
+    srcUrl = srcUrl.replace(skypack_base, "");
+    console.log(srcUrl);
+
+    /// split
+
+    let splitted = srcUrl.split('/');
+    console.log(splitted);
+
+    const v2 = splitted.join('/');
+    console.log(v2);
+
+    cdnUrl = `${process.env.CDN_BASE_URL}/npm/${v2}/+esm`;
+    console.log(cdnUrl);
+
+    const respData = { success: true, url: cdnUrl };
+    res.send(respData);
+})
+
+router.get('/esmsh', async (req, res) => {
+
+    let srcUrl = req.query.url;
+    let cdnUrl = '';
+
+    console.log('parsing esmsh');
+
+    if (srcUrl === undefined || srcUrl === '') {
+        res.send({ success: false });
+        return;
+    }
+
+    if (!srcUrl.startsWith(esmsh_base)) {
+        res.send({ success: false });
+        return;
+    }
+
+    /// remove github entry
+    srcUrl = srcUrl.replace(esmsh_base, "");
+    console.log(srcUrl);
+
+    /// split
+
+    let splitted = srcUrl.split('/');
+    console.log(splitted);
+
+    const v2 = splitted.join('/');
+    console.log(v2);
+
+    cdnUrl = `${process.env.CDN_BASE_URL}/npm/${v2}/+esm`;
+    console.log(cdnUrl);
+
+    const respData = { success: true, url: cdnUrl };
+    res.send(respData);
+})
 
 export default router;
---
+++ utils/google-hosted-libraries.js
@@ -0,0 +1,122 @@
+export const google_hostes_libraries = [
+    {
+        name: 'cesiumjs',
+        npm_name: 'cesiumjs',
+        entrypoints: [
+            { google: 'Build/Cesium/Cesium.js', cdn: 'Build/Cesium/Cesium.js' }
+        ]
+    },
+    {
+        name: 'd3js',
+        npm_name: 'd3',
+        entrypoints: [
+            { google: 'd3.min.js', cdn: 'dist/d3.min.js' }
+        ]
+    },
+    {
+        name: 'dojo',
+        npm_name: 'dojo',
+        entrypoints: [
+            { google: 'dojo/dojo.js', cdn: 'main.min.js' }
+        ]
+    },
+    {
+        name: 'ext-core',
+        npm_name: 'extcore',
+        entrypoints: [
+            { google: 'ext-core.js', cdn: 'index.min.js' }
+        ]
+    },
+    {
+        name: 'hammerjs',
+        npm_name: 'hammerjs',
+        entrypoints: [
+            { google: 'hammer.min.js', cdn: 'hammer.min.js' }
+        ]
+    },
+    {
+        name: 'indefinite-observable',
+        npm_name: 'indefinite-observable',
+        entrypoints: [
+            { google: 'indefinite-observable.bundle.js', cdn: 'dist/indefinite-observable.bundle.js' }
+        ]
+    },
+    {
+        name: 'jquery',
+        npm_name: 'jquery',
+        entrypoints: [
+            { google: 'jquery.min.js', cdn: 'dist/jquery.min.js' }
+        ]
+    },
+    {
+        name: 'jquerymobile',
+        npm_name: 'jquery-mobile',
+        entrypoints: [
+            { google: 'jquerymobile.min.js', cdn: 'js/core.min.js' }
+        ]
+    },
+    {
+        name: 'jqueryui',
+        npm_name: 'jquery-ui',
+        entrypoints: [
+            { google: 'jquery-ui.min.js', cdn: 'dist/jquery-ui.min.js' }
+        ]
+    },
+    {
+        name: 'listjs',
+        npm_name: 'list.js',
+        entrypoints: [
+            { google: 'list.min.js', cdn: 'dist/list.min.js' }
+        ]
+    },
+    {
+        name: 'material-motion',
+        npm_name: 'material-motion',
+        entrypoints: [
+            { google: 'material-motion.bundle.js', cdn: 'dist/material-motion.bundle.js' }
+        ]
+    },
+    {
+        name: 'mobile-viewer',
+        npm_name: 'mobileviewer',
+        entrypoints: [
+            { google: 'mobile-viewer.min.js', cdn: 'index.min.js' }
+        ]
+    },
+    {
+        name: 'mootools',
+        npm_name: 'mootools',
+        entrypoints: [
+            { google: 'mootools.min.js', cdn: '' }
+        ]
+    },
+    {
+        name: 'myanmar-tools',
+        npm_name: 'mobileviewer',
+        entrypoints: [
+            { google: 'mobile-viewer.min.js', cdn: 'index.min.js' }
+        ]
+    },
+    {
+        name: 'prototype',
+        npm_name: 'prototype',
+        entrypoints: [
+            { google: 'prototype.js', cdn: 'lib/Prototype.js' }
+        ]
+    },
+    {
+        name: 'threejs',
+        npm_name: 'three.js',
+        entrypoints: [
+            { google: 'three.min.js', cdn: 'shim.min.js' }
+        ]
+    },
+    {
+        name: 'webfont',
+        npm_name: 'webfont',
+        entrypoints: [
+            { google: 'webfont.js', cdn: 'dist/index.min.js' }
+        ]
+    }
+    
+]
